f1230a1813199349c90728f8050182049dd2970c
Shan John <shanjohn5308@gmail.com>
2023-10-29 10:19:29 +0530

    up

--- lib/widget/contactSction.dart
+++ lib/widget/contactSction.dart
@@ -11,39 +11,41 @@ class ContactSection extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     final TextEditingController messageController = TextEditingController();
  final TextEditingController emailController = TextEditingController();
-    return Container(
-      color: Colors.red,
-      child: Column(
-        children: [
-          Kheight(),
-          Text(
-            "Contact Me",
-            style: ktextstyleopenSans(fontsize: size.width / 20),
-          ), 
-          textfeild(size,emailController,"email"),
-          Kheight(),
-          textfeild(size,messageController,"Message"),
-          Kheight(),
-          TextButton.icon(
-            onPressed: () {
-              if (messageController.text.isNotEmpty&& emailController.text.isNotEmpty) {
-                 Launch.instance.launchEmail();
-              }
-             
-            },
-            icon: Icon(Icons.send),
-            label: Text("Sent"),
-            style: ButtonStyle(
-              
-              shape: MaterialStateProperty.all(BeveledRectangleBorder()),
-              backgroundColor: MaterialStateProperty.all(
-                  const Color.fromARGB(255, 44, 7, 255)),
+    return Expanded(
+      child: Container(
+        color: Colors.purple,
+        child: Column(
+          children: [
+            Kheight(),
+            Text(
+              "Contact Me",
+              style: ktextstyleopenSans(fontsize: size.width / 20),
+            ), 
+            textfeild(size,emailController,"email"),
+            Kheight(),
+            textfeild(size,messageController,"Message"),
+            Kheight(),
+            TextButton.icon(
+              onPressed: () {
+                if (messageController.text.isNotEmpty&& emailController.text.isNotEmpty) {
+                   Launch.instance.launchEmail();
+                }
+               
+              },
+              icon: Icon(Icons.send),
+              label: Text("Sent"),
+              style: ButtonStyle(
+                
+                shape: MaterialStateProperty.all(BeveledRectangleBorder()),
+                backgroundColor: MaterialStateProperty.all(
+                    const Color.fromARGB(255, 44, 7, 255)),
+              ),
             ),
-          ),
-          Kheight(),
-          
-        ],
-      
+            Kheight(),
+            
+          ],
+        
+        ),
       ),
     );
   }
@@ -59,7 +61,7 @@ class ContactSection extends StatelessWidget {
                
                 focusedBorder: OutlineInputBorder(borderSide: BorderSide(color:const Color.fromARGB(151, 0, 0, 0) ,), borderRadius: BorderRadius.all(Radius.circular(50)),),
                  
-                    border: OutlineInputBorder(
+                    border: const OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(50)),
                       borderSide: BorderSide(width: 1),
                     ),
