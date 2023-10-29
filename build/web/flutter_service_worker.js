54c2a514164c7d4f5c4b6df611992341e65747cc
Shan John <shanjohn5308@gmail.com>
2023-10-29 10:09:44 +0530

    up

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "bb7cc83139be83fc35a3725d276cb024",
-"/": "bb7cc83139be83fc35a3725d276cb024",
-"main.dart.js": "20b380f479bd7cfb1451b97fc1ece51e",
+"index.html": "dbcd1e56d091df1e0fb1008df2ceebde",
+"/": "dbcd1e56d091df1e0fb1008df2ceebde",
+"main.dart.js": "353d5c6bf6dd1bacde7c8d420ff833a3",
 "manifest.json": "eb9d7edea85af7232379ccfec3971bc2",
 "version.json": "8f5cf3b0f48fbc2d0d5b1799740aa1ea"};
 // The application shell files that are downloaded before a service worker can
--- lib/widget/contactSction.dart
+++ lib/widget/contactSction.dart
@@ -10,42 +10,38 @@ class ContactSection extends StatelessWidget {
   ContactSection({super.key});
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Container(
-       // color: Colors.amber,
-        width: size.width,
-     decoration: const BoxDecoration( color: Colors.amber,  borderRadius: BorderRadius.only( topLeft:    Radius.circular(50),topRight:  Radius.circular(50)),), 
-        child: Column(
-          children: [
-            Kheight(),
-            Text(
-              "Contact Me",
-              style: ktextstyleopenSans(fontsize: size.width / 20),
-            ), 
-            textfeild(size,emailController,"email"),
-            Kheight(),
-            textfeild(size,messageController,"Message"),
-            Kheight(),
-            TextButton.icon(
-              onPressed: () {
-                if (messageController.text.isNotEmpty&& emailController.text.isNotEmpty) {
-                   Launch.instance.launchEmail();
-                }
-               
-              },
-              icon: Icon(Icons.send),
-              label: Text("Sent"),
-              style: ButtonStyle(
-                
-                shape: MaterialStateProperty.all(BeveledRectangleBorder()),
-                backgroundColor: MaterialStateProperty.all(
-                    const Color.fromARGB(255, 44, 7, 255)),
-              ),
-            ),
-            Kheight(),
+    return Column(
+      children: [
+        Kheight(),
+        Text(
+          "Contact Me",
+          style: ktextstyleopenSans(fontsize: size.width / 20),
+        ), 
+        textfeild(size,emailController,"email"),
+        Kheight(),
+        textfeild(size,messageController,"Message"),
+        Kheight(),
+        TextButton.icon(
+          onPressed: () {
+            if (messageController.text.isNotEmpty&& emailController.text.isNotEmpty) {
+               Launch.instance.launchEmail();
+            }
+           
+          },
+          icon: Icon(Icons.send),
+          label: Text("Sent"),
+          style: ButtonStyle(
             
-          ],
+            shape: MaterialStateProperty.all(BeveledRectangleBorder()),
+            backgroundColor: MaterialStateProperty.all(
+                const Color.fromARGB(255, 44, 7, 255)),
+          ),
+        ),
+        Kheight(),
+        
+      ],
 
-        ));
+    );
   }
 
   SizedBox textfeild(Size size,TextEditingController messageController,String hint ) {
