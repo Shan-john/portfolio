890df2326cd1dfb68aaa45b11799737286acb4d4
Shan John <shanjohn5308@gmail.com>
2023-11-11 17:29:05 +0530

    up

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -41,9 +41,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "8a0c879e98b1eea354109916923f3e60
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "aebe29c20bb279c79f67395297893944",
-"/": "aebe29c20bb279c79f67395297893944",
-"main.dart.js": "55c35f8a5a51f5b65b95025bb01d2700",
+"index.html": "0f04a229bd417be72983111c650631c1",
+"/": "0f04a229bd417be72983111c650631c1",
+"main.dart.js": "d215fb38d11c7e19a28dc9d4a3f80544",
 "manifest.json": "eb9d7edea85af7232379ccfec3971bc2",
 "version.json": "8f5cf3b0f48fbc2d0d5b1799740aa1ea"};
 // The application shell files that are downloaded before a service worker can
--- lib/widget/contactSction.dart
+++ lib/widget/contactSction.dart
@@ -7,61 +7,6 @@ import 'package:protfolio/core/commonFunc.dart';
 import 'package:protfolio/core/constant.dart';
 import 'package:protfolio/core/function.dart';
 
-// class ContactSection extends StatelessWidget {
-//   const ContactSection({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     final size = MediaQuery.of(context).size;
-
-//     return Column(
-//      // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//       crossAxisAlignment: CrossAxisAlignment.center,
-//       children: [
-//         Kheight(),
-//         Text(
-//           "Contact Me",
-//           style: ktextstyleopenSans(fontsize: size.width / 20),
-//         ),
-//         Kheight(),
-//         Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//           children: [
-//             Icon(
-//               Icons.email,
-//               size: size.width / 20,
-//               color: Colors.red[400],
-//             ),
-//         Icon(FontAwesome.whatsapp, size: size.width / 20, color: Colors.green[400],),
-//             Icon(FontAwesome.github_squared, size: size.width / 20, color: Color.fromARGB(255, 255, 255, 255),)
-//           ],
-//         ),
-//       ],
-//     );
-//   }
-// }
-
-// class ContactSection extends StatelessWidget {
-//   const ContactSection({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     final size = MediaQuery.of(context).size;
-
-//     return Container(
-//       height: 60,
-//       color: Colors.red,
-//       child: Column(
-//           // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: [
-//             Padding(
-//               padding: const EdgeInsets.all(8.0),
-//               child: CupertinoTextField(autocorrect: true,controller: ),
-//             )
-             
-//           ]),
-//     );
-//   }
-// }
 
 class ContactSection extends StatelessWidget {
   @override
